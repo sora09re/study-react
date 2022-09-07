7b3bbe47b8adbfaa62be5d2b8150839909b1a864
bluesolar1999 <blue.solar1999@gmail.com>
2022-09-08 07:35:16 +0900

    ♻️ export default の削除

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Footer from "../components/Footer";
-import Links from "../components/Links";
-import Headline from "../components/Headline";
+import { Footer } from "../components/Footer";
+import { Links } from "../components/Links";
+import { Headline } from "../components/Headline";
 
 export default function Home() {
   return (
