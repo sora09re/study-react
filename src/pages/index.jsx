963f8d0495420ca0ca71338e4907b91cb07ca254
bluesolar1999 <blue.solar1999@gmail.com>
2022-09-12 19:46:27 +0900

    ⚗️ クリックイベントのテスト記述

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,23 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useCallback } from "react";
+
+// const handleClick = (e) => {
+//   console.log(e.target.href);
+//   e.preventDefault();
+//   alert(foo);
+// }
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    onsole.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +27,9 @@ export default function Home() {
       </Head>
 
       <Header />
-
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
       <Footer />
     </div>
