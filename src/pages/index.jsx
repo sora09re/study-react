3f1c5e36cb1941799a9de11e46e58c2e9244971b
bluesolar1999 <blue.solar1999@gmail.com>
2022-09-14 07:59:42 +0900

    ✨ useStateで文字列と真偽値について勉強

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,25 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [foo, setFoo] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = (e) => {
     setFoo((foo) => foo + 1);
     setFoo((foo) => foo + 1);
   };
+  
+  const handleChange = useCallback((e) => {
+    setText(e.target.value);
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
 
   useEffect(() => {
     document.body.style.backgroundColor = "gray";
@@ -27,8 +37,10 @@ export default function Home() {
       </Head>
 
       <Header />
-      <h1>{foo}</h1>
+      {isShow ? <h1>{foo}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
     </div>
