e526dfb3892d52eae51ac68ba0fddccf15a85895
bluesolar1999 <blue.solar1999@gmail.com>
2022-09-27 22:19:06 +0900

    ⚡️ useReducerを使用した

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,28 +5,6 @@ import { Posts } from "src/components/Posts";
 import { useCallback, useEffect, useState } from "react";
 
 const Home = (props) => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  const getPosts = useCallback(async () => {
-    try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/postsfda");
-      if (!res.ok) {
-        throw new Error("エラーが発生したため、データの取得に失敗しました。");
-      }
-      const json = await res.json();
-      setPosts(json);
-    } catch (error) {
-      setError(error);
-    }
-    setLoading(false);
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
   return (
     <div className={styles.container}>
       <Head>
