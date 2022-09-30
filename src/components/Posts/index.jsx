6dabf92512409b1348bb52f6b537c8bef7a61626
bluesolar1999 <blue.solar1999@gmail.com>
2022-10-01 07:18:27 +0900

    🚚 ファイルシステムルーティングを勉強

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { usePosts } from "src/hooks/usePosts";
 
 export const Posts = () => {
@@ -17,7 +18,13 @@ export const Posts = () => {
   return (
     <ol>
       {data.map((post) => {
-        return <li key={post.id}>{post.title}</li>;
+        return (
+          <li key={post.id}>
+            <Link href={`/post/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        );
       })}
     </ol>
   );
