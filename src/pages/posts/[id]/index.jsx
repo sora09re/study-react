c837a451eab7f6cceed2ce6476a9ecb53987ada1
bluesolar1999 <blue.solar1999@gmail.com>
2022-10-01 14:18:56 +0900

    🎨 Layoutをコンポーネントを使ってまとめた

--- src/pages/posts/[id]/index.jsx
+++ src/pages/posts/[id]/index.jsx
@@ -1,15 +1,12 @@
-import Head from "next/head";
-import styles from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
-import { usePost } from "src/hooks/usePost";
 import { Post } from "src/components/Post";
 
 const PostsId = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Header />
       <Post />
     </div>
   );
 };
-export default PostId;
+export default PostsId;
