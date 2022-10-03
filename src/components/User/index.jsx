f5a944d159970493e5e08ba7135f297cb1d84f2f
bluesolar1999 <blue.solar1999@gmail.com>
2022-10-04 08:28:06 +0900

    ✨ ユーザーページに投稿一覧を表示

--- src/components/User/index.jsx
+++ src/components/User/index.jsx
@@ -1,3 +1,4 @@
+import { PostsByUserId } from "src/components/Posts/PostsByUserId";
 import { useUser } from "src/hooks/useUser";
 
 export const User = () => {
@@ -13,6 +14,7 @@ export const User = () => {
   return (
     <div>
       <h1>{data.name}</h1>
+      <h2>詳細</h2>
       <ul>
         <li>{data.email}</li>
         <li>{data.username}</li>
@@ -21,6 +23,9 @@ export const User = () => {
         <li>{data.website}</li>
         <li>{data.company.name}</li>
       </ul>
+      <h2>投稿</h2>
+      <PostsByUserId id={data.id} />
+      <h2>コメント</h2>
     </div>
   );
 };
